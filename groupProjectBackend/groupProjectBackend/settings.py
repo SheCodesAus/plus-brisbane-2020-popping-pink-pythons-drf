414c7700f536cd799839abb4ebc67c925645966b
Natasha Seeto <tashseeto@gmail.com>
2020-11-17 20:18:45 +1000

    BE-001-U - create user model.py & updated settings.py

--- groupProjectBackend/groupProjectBackend/settings.py
+++ groupProjectBackend/groupProjectBackend/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'opportunity.apps.OpportunityConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'users.CustomUser'
