9c0380a31116a09437158690499d1f6bf537f955
gianninapg@gmail.com <Gi@090488>
2020-11-21 12:05:11 +1000

    updated url.py for opportunity

--- groupProjectBackend/opportunity/urls.py
+++ groupProjectBackend/opportunity/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('opportunity/', views.OpportunityList.as_view()),
     path('opportunity/<int:pk>/', views.OpportunityDetail.as_view()),
-    path('/favourites/', views.OpportunityLatest.as_view())
+    path('opportunity/latest/', views.OpportunityLatest.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
