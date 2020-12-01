8833ee7a7b73ea03efd1c6220bcff4cd5f61e7fd
Natasha Seeto <tashseeto@gmail.com>
2020-12-01 19:02:16 +1000

    Added CurrentUserView

--- groupProjectBackend/users/urls.py
+++ groupProjectBackend/users/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('users/', views.CustomUserList.as_view()),
     path('users/<int:pk>/', views.CustomUserDetail.as_view()),
+    path('users/currentuser/', views.CurrentUserView.as_view()),
     path('favourites/', views.UserFavouriteView.as_view()),
 ]
 
